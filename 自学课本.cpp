3b133b67012ddc54806c70c2a3b5892c27828db4
cookie250709 <bingweizhao2@gmail.com>
2025-10-15 16:42:55 +0800

    学会了二维数组好心开

--- 自学课本.cpp
+++ 自学课本.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
-using namespace std ;
+using namespace std;
 
-int maxx(int x,int y){
-  if(x>=y){return x;}
-  else {return y;}
-}
-
-int main()
-{
-    int x,y;
-    cin>>x>>y;
-  cout<<maxx(x,y)<<endl;
-
-  return 0;
+int main(){
+  int arr[3][3];
+  int n=9;
+  int temp=0;
+  float aver;
+  cout<<"请输入您要输入的9个数据"<<endl;
+  for(int i=0;i<3;i++){
+    for(int j=0;j<3;j++){
+        cin>>*(*(arr+i)+j);
+    }
   }
+  for(int i=0;i<3;i++){
+    for(int j=0;j<3;j++){
+       cout<<*(*(arr+i)+j)<<" ";
+       temp+=*(*(arr+i)+j);
+    }
+    cout<<endl;
+  }
+  aver=(float)temp/n;
+  cout<<"平均值是："<<aver<<endl;
 
+
+return 0;
+}
